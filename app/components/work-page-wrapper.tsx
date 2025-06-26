2c3f772568deb2866cd4fbaa271bd40499c5403e
Andrew Ambrosino <a@ambrosino.io>
2025-06-26 11:21:42 -0400

    fix build

--- app/components/work-page-wrapper.tsx
+++ app/components/work-page-wrapper.tsx
@@ -24,10 +24,7 @@ export function WorkPageWrapper({ title, children, description }: WorkPageWrappe
 
   return (
     <>
-      <div
-        idx={0}
-        className="p-pageMargin align-stretch from-background/100 via-background/75 group/nav top-pageMargin sticky top-0 z-50 mx-auto grid translate-z-0 grid-cols-[1fr_auto_1fr] items-center bg-gradient-to-b transition-colors duration-1000 hover:duration-250 md:p-[max(var(--padding-pageMargin),5vh)]"
-      >
+      <div className="p-pageMargin align-stretch from-background/100 via-background/75 group/nav top-pageMargin sticky top-0 z-50 mx-auto grid translate-z-0 grid-cols-[1fr_auto_1fr] items-center bg-gradient-to-b transition-colors duration-1000 hover:duration-250 md:p-[max(var(--padding-pageMargin),5vh)]">
         <div className="-ml-1.5 sm:-ml-1.5">
           <LinkButton href="/" className="rounded-full" icon={<IconChevronLeft className="iconSize" />} label="All" />
         </div>
