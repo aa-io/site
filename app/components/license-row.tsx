2c1f609400c944f7171da3bfb60460c5b91cfc3c
Andrew Ambrosino <a@ambrosino.io>
2025-06-26 23:51:40 -0400

    improve responsiveness

--- app/components/license-row.tsx
+++ app/components/license-row.tsx
@@ -10,14 +10,14 @@ export const LicenseRow = ({
   number: string;
 }) => {
   return (
-    <div className="group/license text-foreground/65 flex flex-col py-1.5 text-sm leading-relaxed">
-      <div className="group-hover/mention:text-foreground grow gap-1.5 md:flex">
+    <div className="group/license text-foreground/65 flex flex-col py-1.5 text-sm leading-relaxed sm:flex-row sm:gap-3">
+      <div className="group-hover/mention:text-foreground gap-1.5 md:flex">
         <div className="line-clamp-1 truncate">{name}</div>
       </div>
 
-      <div className="text-muted-foreground flex shrink-0 grow-0 gap-1 tabular-nums">
+      <div className="text-muted-foreground flex shrink-0 grow gap-1 tabular-nums">
         <div className="text-muted-foreground grow truncate opacity-65">{entity}</div>
-        <div className="flex shrink-0 gap-1">
+        <div className="flex shrink-0 grow justify-end gap-1 text-right">
           <div className="opacity-65">{credential}</div> <div className="line-clamp-1">{number}</div>
         </div>
       </div>
