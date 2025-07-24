80a60712cff0df01eb8c6d297ef7178636eb5793
Andrew Ambrosino <a@ambrosino.io>
2025-07-23 22:10:53 -0400

    fix for safarri

--- app/(pages)/chat/page.tsx
+++ app/(pages)/chat/page.tsx
@@ -8,7 +8,7 @@ import { LinkButton } from '../../components/social-link';
 export default function ChatPage() {
   return (
     <>
-      <div className="p-pageMargin align-stretch from-background/100 via-background/75 group/nav top-pageMargin fixed sticky top-0 z-50 mx-auto grid translate-z-0 grid-cols-[1fr_auto_1fr] items-center bg-gradient-to-b transition-colors duration-1000 hover:duration-250 md:p-[max(var(--padding-pageMargin),5vh)]">
+      <div className="align-stretch from-background/100 via-background/75 group/nav top-pageMargin p-pageMargin fixed top-0 z-50 mx-auto grid translate-z-0 grid-cols-[1fr_auto_1fr] items-center bg-gradient-to-b transition-colors duration-1000 hover:duration-250 md:p-[max(var(--padding-pageMargin),5vh)]">
         <div className="sm:-ml-1.5">
           <LinkButton href="/" className="rounded-full" icon={<IconChevronLeft className="iconSize" />} label="Home" />
         </div>
